8abea58cec7815970b980d757d2568357f08375e
Jancee Rod <jancee800@gmail.com>
2024-01-29 17:47:07 +0800

    add functionality where scores are displayed

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
 
+let playerScoreText = document.getElementById('player-score');
+playerScoreText.textContent = "Player: " + playerScore;
+
+let computerScoreText = document.getElementById('computer-score');
+computerScoreText.textContent = "Computer: " + computerScore;
+
 const rockButton = document.getElementById('rock-button');
 const paperButton = document.getElementById('paper-button');
 const scissorsButton = document.getElementById('scissors-button');
@@ -18,18 +24,21 @@ function game() {
         playerImage.src = "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/270a.svg";
         playerChoice = 'rock';
         computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice)
     });
 
     paperButton.addEventListener('click', () => {
         playerImage.src = "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/270b.svg";
         playerChoice = 'paper';
         computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice)
     });
 
     scissorsButton.addEventListener('click', () => {
         playerImage.src = "https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/svg/270c.svg";
         playerChoice = 'scissor';
         computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice)
     });
 
     console.log(playerScore);
@@ -40,26 +49,32 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice == "rock" && computerChoice == "paper") {
         console.log("Computer Wins!");
         computerScore += 1;
+        computerScoreText.textContent = "Computer: " + computerScore;
     }
     else if (playerChoice == "paper" && computerChoice == "rock") {
         console.log("Player Wins!");
         playerScore += 1;
+        playerScoreText.textContent = "Player: " + playerScore;
     }
     else if (playerChoice == "rock" && computerChoice == "scissor") {
         console.log("Player Wins!");
         playerScore += 1;
+        playerScoreText.textContent = "Player: " + playerScore;
     }
     else if (playerChoice == "scissor" && computerChoice == "rock") {
         console.log("Computer Wins!");
         computerScore += 1;
+        computerScoreText.textContent = "Computer: " + computerScore;
     }
     else if (playerChoice == "paper" && computerChoice == "scissor") {
         console.log("Computer Wins!");
         computerScore += 1;
+        computerScoreText.textContent = "Computer: " + computerScore;
     }
     else if (playerChoice == "scissor" && computerChoice == "paper") {
         console.log("Player Wins!");
         playerScore += 1;
+        playerScoreText.textContent = "Player: " + playerScore;
     }
     else if (playerChoice == computerChoice) {
         console.log("Its a Tie!");
